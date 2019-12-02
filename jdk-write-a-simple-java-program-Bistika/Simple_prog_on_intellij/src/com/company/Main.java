8c6412fe47ca9987d36234141c78fbbbfd438062
Bistika <marius.bistika@gmail.com>
2019-12-02 20:24:09 +0200

    This commit contains both simple programs without external library.

---
+++ jdk-write-a-simple-java-program-Bistika/Simple_prog_on_intellij/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("This one was created and ran on Intellij.");
+    }
+}
---
+++ jdk-write-a-simple-java-program-Bistika/Simple_prog_ran_by_console/helloworld.java
@@ -0,0 +1,5 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println ("Hello, this is the first Java program written, compiled and ran through terminal.");
+    }
+}
